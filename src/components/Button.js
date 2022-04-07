657e91ebabcae52c2cc39283f0ce513d45d2454f
Trey K <80800860+treykapfer@users.noreply.github.com>
2022-04-06 20:01:23 -0500

    add click

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,23 @@
+import PropTypes from 'prop-types'
 
 const Button = (props) => {
+    const onClick = () => {
+        console.log('click')
+    }
     return(
-        <button style={{backgroundColor: props.color}}className='btn'>{props.text}</button>
+        <button onClick={onClick}style={{backgroundColor: props.color}}className='btn'>
+            {props.text}
+        </button>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default Button;
