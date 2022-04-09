f1676f3b866476b73c0dc7728e55cc8f915581c2
Trey K <80800860+treykapfer@users.noreply.github.com>
2022-04-09 00:48:11 -0500

    add task form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,7 +2,24 @@
 const AddTask = () => {
 
     return (
-        <form>
+        <form className='add-form'>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task' />
+            </div>
+            
+            <div className='form-control'>
+                <label>Day and Time</label>
+                <input type='text' placeholder='Add Day and Time' />
+            </div>
+            
+            <div className='form-control'>
+                <label>Set Reminder</label>
+                <input type='checkbox' />
+            </div>
+
+            <input type='submit' value='Save Task'/>
+
         </form>
     )
 }
