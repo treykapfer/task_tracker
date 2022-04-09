130299a56f31fa57379240112e61d6e41357d156
Trey K <80800860+treykapfer@users.noreply.github.com>
2022-04-09 00:56:15 -0500

    add task stuff

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,27 +1,42 @@
+import { useState } from 'react'
 
 const AddTask = () => {
+
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const textChangeHandler = (e) => {
+        setText(e.target.value)
+    }
+    const dayChangeHandler = (e) => {
+        setDay(e.target.value)
+    }
+    const reminderChangeHandler = (e) => {
+        setReminder(e.currentTarget.checked)
+    }
 
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task' />
+                <input type='text' placeholder='Add Task' value={text} onChange={textChangeHandler} />
             </div>
             
             <div className='form-control'>
                 <label>Day and Time</label>
-                <input type='text' placeholder='Add Day and Time' />
-            </div>
-            
-            <div className='form-control'>
-                <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='text' placeholder='Add Day and Time' value={day} onChange={dayChangeHandler}/>
             </div>
 
-            <input type='submit' value='Save Task'/>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' value={reminder} onChange={reminderChangeHandler}/>
+            </div>
+
+            <input className='btn btn-block' type='submit' value='Save Task'/>
 
         </form>
     )
 }
 
-export default Task;
+export default AddTask;
